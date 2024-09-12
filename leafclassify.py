3066c637724c227f7653e2918e4ea2043488fe62
Mighty A. Ayidzoe <mighty.ayidzoe@uds.edu.gh>
2024-09-12 13:37:02 +0000

    Update leafclassify.py

--- leafclassify.py
+++ leafclassify.py
@@ -15,7 +15,8 @@ import base64
 
 
 # fig = plt.figure()
-st.title("<h1 style='color: red;'>AI4AFS-UENR</h1>")
+#st.title("<h1 style='color: red;'>AI4AFS-UENR</h1>")
+st.markdown("<h1 style='color: red;'>AI4AFS-UENR</h1>", unsafe_allow_html=True)
 st.header('cassava Disease/Pest Detection App')
 
 #st.markdown("Prediction Platform")
